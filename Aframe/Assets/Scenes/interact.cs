c0877bac88c87d8f4bd975ed152ef47cf1f5e42f
Akshay Gupta <38081657+Trance-raver@users.noreply.github.com>
2018-12-04 18:00:52 +0530

    ExampleModel

---
+++ Aframe/Assets/Scenes/interact.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class interact : MonoBehaviour {
+
+    public BoxCollider cd;
+	// Use this for initialization
+	void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider2D other)
+    {
+        if (other.gameObject.tag == "camera")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
+
---
+++ Aframe/Assets/test.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class test : MonoBehaviour {
+
+    // not used for WebVr A-frame
+
+    GameObject game;
+    public Text text;
+    public float speed;
+    private Rigidbody rb;
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("yes");
+            //Vector3 movement = new Vector3(0,1f,0);
+            // rb.AddForce(movement * speed);
+            this.transform.Translate(Vector3.right * speed * Time.deltaTime);
+
+        }
+	}
+}
